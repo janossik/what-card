b5d57fc00cb26aa5bbe5b58b94ec8d078c98323b
janossik <marcinczaniecki97@gmail.com>
2020-12-10 14:41:25 +0100

    heroku work 1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const open = require("open");
 const verif = require("./js/whatCard");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // parse application/json
 app.use(bodyParser.json());
